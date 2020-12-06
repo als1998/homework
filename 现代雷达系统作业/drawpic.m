348bd1ab9c7d3dcdaa4c55fddf7fc398a4a68531
zerzeg <1364333287@qq.com>
2020-12-06 20:50:20 +0800

    添加注释

--- 现代雷达系统作业/drawpic.m
+++ 现代雷达系统作业/drawpic.m
@@ -3,7 +3,7 @@ function  picdata=drawpic(inputArg1)
     picsize=size(rawdata);
     rawdata=fix((rawdata+abs(min(min(rawdata)))).*picsize(1)./(max(max(rawdata))-min(min(rawdata))));
     picdata=zeros(picsize);
-    
+    %%将每一列中的数字换算成每一行中所对应位置处的像素
     for j=1:picsize(2)
         for i=1:picsize(1)
             picdata(max(max([rawdata(i,j),1])),j)=picdata(max(max([rawdata(i,j),1])),j)+1;
